5cff19256792d13a86a5380b4b2f6c5e1991c22c
z7x8c9 <159719335+z7x8c9@users.noreply.github.com>
2024-12-01 14:13:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function createStar() {
+    const star = document.createElement('div');
+    star.className = 'star';
+    star.style.left = `${Math.random() * 100}vw`;
+    star.style.animationDuration = `${Math.random() * 3 + 2}s`;
+    document.body.appendChild(star);
+
+    setTimeout(() => {
+        star.remove();
+    }, 5000);
+}
+
+setInterval(createStar, 100);
